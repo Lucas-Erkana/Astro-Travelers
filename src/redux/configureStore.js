560589088e1ec709988459ffe166d0dcc94d6c99
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-30 18:18:13 +0200

    Update configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import missionReducer from './missions/mission';
 import rocketSlice from './rockets/rocketSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketSlice,
+    missions: missionReducer,
 
   },
 });
