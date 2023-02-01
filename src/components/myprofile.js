a77c6a699e93ade274cbe3629c5d80579faebc8c
Mukama Steven <stevenmukama1@gmail.com>
2023-02-01 07:19:33 +0200

    use filter

--- src/components/myprofile.js
+++ src/components/myprofile.js
@@ -8,28 +8,25 @@ const Myprofile = () => {
   const missionsData = useSelector((state) => state.missions);
   const rocketsData = useSelector((state) => state.rockets);
 
+  const reservedRockets = rocketsData.filter((rocket) => rocket.reserved);
+  const joinedMissions = missionsData.filter((mission) => mission.reserved);
+
   return (
     <div className={styles.profile_container}>
       <div className="rocket-contain">
         <h2>My Rockets</h2>
         <ul className="table1">
-          {rocketsData.map((rocket) => {
-            if (rocket.reserved) {
-              return <li className="data" key={rocket.id}>{rocket.name}</li>;
-            }
-            return '';
-          })}
+          {reservedRockets.map((rocket) => (
+            <li className="data" key={rocket.id}>{rocket.name}</li>
+          ))}
         </ul>
       </div>
       <div className="container">
         <h2 className="Mymish">My Missions</h2>
         <ul className="containers">
-          {missionsData.map((mission) => {
-            if (mission.reserved) {
-              return <li className="missionList" key={mission.id}>{mission.name}</li>;
-            }
-            return '';
-          })}
+          {joinedMissions.map((mission) => (
+            <li className="missionList" key={mission.id}>{mission.name}</li>
+          ))}
         </ul>
       </div>
     </div>
